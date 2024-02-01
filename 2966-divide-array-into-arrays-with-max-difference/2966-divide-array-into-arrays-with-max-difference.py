c995ab282008466a51a817d2e1fc3d32734e2536
Alekhya <96313151+Alekhyadantuluri@users.noreply.github.com>
2024-02-01 21:08:09 +0530

    Time: 703 ms (95.05%), Space: 32.2 MB (26.27%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
@@ -4,7 +4,7 @@ class Solution:
         l=[]
         for i in range(0,len(nums)-2,3):
             a=[nums[i]]
-            if nums[i+1]-nums[i] <=k and nums[i+2]-nums[i+1]<=k and len(a)!=0 and nums[i+2]-nums[i]<=k:
+            if nums[i+1]-nums[i] <=k and nums[i+2]-nums[i+1]<=k and nums[i+2]-nums[i]<=k:
                 a.append(nums[i+1])
                 a.append(nums[i+2])
             else:
